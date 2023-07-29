00efc6a772f20ab065ceddbdd40f67bcde62945a
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-07-29 11:17:30 -0400

    fix: replace endpoint to router

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 const port = 3000
 
-app.use('/', function (req, res) {
-    res.send('Hello World');
+app.use(router);
+
+router.get('/', function (req, res) {
+  res.send('Hello World');
 });
+
+// app.use('/', function (req, res) {
+//     res.send('Hello World');
+// });
 
 app.listen(port);
 console.log(`Server started on port ${port}`);
