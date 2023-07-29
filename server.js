537342522b0b33d6493e4ba6f926a22f849ffe58
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-07-29 11:31:54 -0400

    feat: body and query add to request delete /message
    used to will example

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
 router.get('/', function (req, res) {
   res.send('Hello World');
+});
+router.get('/messages', function (req, res) {
+  res.send('Lista de message');
+});
+router.delete('/messages', function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
+
+  res.send('Eliminar message');
 });
 
 // app.use('/', function (req, res) {
