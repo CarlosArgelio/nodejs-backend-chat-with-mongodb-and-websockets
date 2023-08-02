a9424479f77d150410e95dbf7f792d56c5deede0
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-02 00:13:33 -0400

    feat; manage connect to db

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,24 +1,4 @@
-const db = require('mongoose');
 const Model = require('./model');
-
-const user = encodeURIComponent('palaciosrcarlosa2')
-const password = encodeURIComponent('AIWrO0oyuVtsVEDy')
-const cluster = 'cluster0.2ez0s8x.mongodb.net'
-const clusterPrimary = 'c-guivwrn-shard-00-00.2ez0s8x.mongodb.net:27017'
-const clusterSecondary = 'ac-guivwrn-shard-00-01.2ez0s8x.mongodb.net:27017'
-const clusterTertiary = 'ac-guivwrn-shard-00-02.2ez0s8x.mongodb.net:27017'
-const nameCollection = 'messages'
-
-const uri =
-  `mongodb://${user}:${password}@${clusterPrimary},${clusterSecondary},${clusterTertiary}/${nameCollection}?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true`;
-
-const uri2 = `mongodb+srv://${user}:${password}@${cluster}/${nameCollection}`
-
-db.Promise = global.Promise;
-
-db.connect(uri2, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'telegrom' })
-  .then(() => console.log('[db] Conectada con éxito'))
-  .catch(err => console.error('[db]', err));
 
 function addMessage(message) {
   const myMessage = new Model(message);
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const router = require('./components/messages/network');
+const db = require('./db');
+
 const router = require('./network/routers');
+
+const user = encodeURIComponent('palaciosrcarlosa2')
+const password = encodeURIComponent('AIWrO0oyuVtsVEDy')
+const cluster = 'cluster0.2ez0s8x.mongodb.net'
+const nameCollection = 'messages'
+const uri = `mongodb+srv://${user}:${password}@${cluster}/${nameCollection}`
+
+db(uri)
 
 var app = express();
 const port = 3000
