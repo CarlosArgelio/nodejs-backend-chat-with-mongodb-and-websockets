df167f5ae84e2b94a26ebcbe8eee54d45022f65d
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-04 18:59:07 -0400

    feat: componets chat

--- network/routers.js
+++ network/routers.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const message = require('../components/messages/network')
 const user = require('../components/user/network')
+const chat = require('../components/chat/network')
 
 const routers = function (server) {
     server.use('/messages', message);
     server.use('/users', user);
+    server.use('/chat', chat);
 }
 
 module.exports = routers;
